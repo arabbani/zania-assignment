0c070c8590d7597c5b2bec4656468d2e96c81f7d
Arif Rabbani <arif.93rabbani@gmail.com>
2024-09-04 11:41:08 +0530

    mantine ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "@mantine/core/styles.css";
+
+import { Container, MantineProvider } from "@mantine/core";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <MantineProvider>
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    </MantineProvider>
+  );
 }
 
 export default App;
