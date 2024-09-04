57cf16de39cc66578f4e3bdc37d9b568214c744a
Arif Rabbani <arif.93rabbani@gmail.com>
2024-09-04 23:46:42 +0530

    added support for reordering of the cards via drag and drop

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,13 @@
 import { Grid } from "@mantine/core";
-import { Card } from "./Card";
+import { useEffect, useRef, useState } from "react";
 import { Item } from "../utils/type";
-import { useEffect, useState } from "react";
+import { Card } from "./Card";
 
 export function Home() {
   const [items, setItems] = useState<Item[]>();
+
+  const draggedItemRef = useRef<number>(0);
+  const draggedOverItemRef = useRef<number>(0);
 
   const sortedItems: Item[] | undefined = items?.sort(
     (itemA, itemB) => itemA.position - itemB.position
@@ -20,10 +23,30 @@ export function Home() {
     getItems();
   }, []);
 
+  const handleDragEnd = () => {
+    const clonedItems = JSON.parse(JSON.stringify(items));
+    const draggedItem = clonedItems[draggedItemRef.current];
+    const draggedOverItem = clonedItems[draggedOverItemRef.current];
+
+    draggedOverItem.position = draggedItemRef.current;
+    draggedItem.position = draggedOverItemRef.current;
+
+    clonedItems[draggedItemRef.current] = draggedOverItem;
+    clonedItems[draggedOverItemRef.current] = draggedItem;
+    setItems(clonedItems);
+  };
+
   return (
     <Grid justify="center">
-      {sortedItems?.map((item) => (
-        <Grid.Col span={4} key={item.position}>
+      {sortedItems?.map((item, index) => (
+        <Grid.Col
+          span={4}
+          key={item.position}
+          draggable
+          onDragStart={() => (draggedItemRef.current = index)}
+          onDragEnter={() => (draggedOverItemRef.current = index)}
+          onDragEnd={handleDragEnd}
+        >
           <Card item={item} />
         </Grid.Col>
       ))}
